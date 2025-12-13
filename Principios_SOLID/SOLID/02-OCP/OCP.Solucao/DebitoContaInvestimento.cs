de8a6a2bf5a1ecc057fa80962068eba5cd12e279
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-13 11:13:55 -0300

    ✨ feat: estudando sobre a solução OCP
    como escrever um código correto utilizando do principio open closed

---
+++ Principios_SOLID/SOLID/02-OCP/OCP.Solucao/DebitoContaInvestimento.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLID._02_OCP.OCP.Solucao
+{
+    public class DebitoContaInvestimento : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            // debitar conta investimento
+            //inserir taxas
+            return NumeroTransacao;
+        }
+    }
+}
---
+++ Principios_SOLID/SOLID/02-OCP/OCP.Solucao/DebitoContaPoupanca.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLID._02_OCP.OCP.Solucao
+{
+    public class DebitoContaPoupanca : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            // valida aniversário da conta poupança
+            // debitar conta poupança
+            return NumeroTransacao;
+        }
+    }
+}
