b7e87037873e8a7fed0f88693680e4a933d5d815
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-15 18:21:02 -0300

    ✨ feat: estudando sobre a solução ISP
    Entendendo que, varias interfaces com cada uma para seu papel, é melhor que uma interface que faz tudo.

---
+++ Principios_SOLID/SOLID/04-ISP/ISP.Solucao/CadastroCliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SOLID._04_ISP.ISP.Solucao
+{
+    public class CadastroCliente : ICadastroCliente
+    {
+        public void EnviarEmail()
+        {
+            throw new NotImplementedException();
+            // envia email ao cliente
+        }
+
+        public void SalvarBanco()
+        {
+            throw new NotImplementedException();
+            // insert no banco
+        }
+
+        public void ValidarDados()
+        {
+            throw new NotImplementedException();
+            // valida os dados
+        }
+    }
+}
---
+++ Principios_SOLID/SOLID/04-ISP/ISP.Solucao/ICadastroCliente.cs
@@ -0,0 +1,9 @@
+﻿namespace SOLID._04_ISP.ISP.Solucao
+{
+    public interface ICadastroCliente
+    {
+        void ValidarDados();
+        void SalvarBanco();
+        void EnviarEmail();
+    }
+}
