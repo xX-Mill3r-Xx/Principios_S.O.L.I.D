1f2729e0e4519198599ac5c3ac7ea1f9aeac257c
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-15 18:15:34 -0300

    ✨ feat: estudando a violação do ISP
    Entendendo sobre o porque de ter varias interfaces e cada uma para uma situação em especifico é sempre melhor que ter apenas uma interface que tenta abranger tudo de uma só vez.

---
+++ Principios_SOLID/SOLID/04-ISP/ISP.Violacao/CadastroCliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SOLID._04_ISP.ISP.Violacao
+{
+    public class CadastroCliente : ICadastro
+    {
+        public void EnviarEmail()
+        {
+            throw new NotImplementedException();
+            // envia email para o cliente
+        }
+
+        public void SalvarBanco()
+        {
+            throw new NotImplementedException();
+            // insert na tabela
+        }
+
+        public void ValidarDados()
+        {
+            throw new NotImplementedException();
+            // valida os dados
+        }
+    }
+}
---
+++ Principios_SOLID/SOLID/04-ISP/ISP.Violacao/ICadastro.cs
@@ -0,0 +1,9 @@
+﻿namespace SOLID._04_ISP.ISP.Violacao
+{
+    public interface ICadastro
+    {
+        void ValidarDados();
+        void SalvarBanco();
+        void EnviarEmail();
+    }
+}
