8ad30f01c7c4b026824b078d7429993bc8121fa5
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-17 09:13:34 -0300

    ✨ feat: entendendo as violações do dip

---
+++ Principios_SOLID/SOLID/05-DIP/DIP.Violacao/ClienteService.cs
@@ -0,0 +1,18 @@
+﻿namespace SOLID._05_DIP.DIP.Violacao
+{
+    public class ClienteService
+    {
+        public string AdicionarCliente(Cliente cliente)
+        {
+            if (!cliente.IsValid())
+                return "Cliente inválido";
+
+            var repo = new ClienteRepository();
+            repo.AdicionarCliente(cliente);
+
+            _01_SRP.SRP.Solucao.EmailServices.Enviar("empresa@empresa.com", cliente.Email, "Bem Vindo", "parabens, está cadastrado");
+
+            return "Cliente adicionado com sucesso";
+        }
+    }
+}
---
+++ Principios_SOLID/SOLID/05-DIP/DIP.Violacao/EmailServices.cs
@@ -0,0 +1,16 @@
+﻿namespace SOLID._05_DIP.DIP.Violacao
+{
+    public class EmailServices
+    {
+        public static bool IsValid(string email)
+        {
+            // lógica de validação de email
+            return email.Contains("@");
+        }
+
+        public static void Enviar(string de, string para, string assunto, string message)
+        {
+            //logica para envio de email
+        }
+    }
+}
